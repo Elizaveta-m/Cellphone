bec8b4be5fdad2d10f3b1e200e8f452feac4ed2d
Elizaveta Morozova <rusquee@gmail.com>
2021-03-12 01:13:16 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-      int income = 1500;
-      int bonusStep = 100;
-      if (income >= 1000) {
-          int bonus = income / bonusStep;
-          System.out.println(bonus);
-      } else {
-          int bonus = 0;
-          System.out.println(bonus);
-      }
+        int income = 1_500;
+        int bonusStep = 100;
+        int bonus;
+        if (income >= 1_000) {
+            bonus = income / bonusStep;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
     }
 }
